ef9c2d05dc47ec98a7470b059f3b69c3d4b8e626
Muhammad Mustafa <75218580+mmustafa93@users.noreply.github.com>
2025-02-03 23:09:48 +0500

    Implement the fibonacci function recursively

--- fibonacci.js
+++ fibonacci.js
@@ -11,5 +11,19 @@ const fibs = (num) => {
     return array
 }
 
+const fibsRec = (num) => {
+    console.log("Recursive")
+    if (num == 0){
+        return [];
+    } else if (num < 3) {
+        return num == 1 ? [0] : [0, 1];
+    } else {
+        const array = fibsRec(num-1);
+        return array.concat(array[array.length - 2] + array[array.length - 1]);
+    }
+}
+
 const result = fibs(8);
+const resultRec = fibsRec(8);
 console.log(result)
+console.log(resultRec)
